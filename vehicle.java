f203e2301b5e2e5d35f84d6c42e93239813b890d
Bakyalakshmi <bakyalakshmi005@gmail.com>
2023-03-06 20:19:23 +0530

    PetrolEngine class  added

--- vehicle.java
+++ vehicle.java
@@ -1,10 +1,10 @@
 public class Vehicle{
 	
-	private PetrolEngine pE;
+
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world")
+PetrolEngine pE=new petrolEngine();
 		pE.start();
 	}
 }
